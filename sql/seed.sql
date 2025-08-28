340719c6dfa2fc5681d6891483bf1c5967b0d0ac
OlgaLeLe <olga.olga-eremina2017@yandex.ru>
2025-08-28 14:59:12 +0300

    Create seed.sql

---
+++ sql/seed.sql
@@ -0,0 +1,12 @@
+-- Стартовые слова (10 слов)
+INSERT INTO words (ru, en, part_of_speech) VALUES
+('красный', 'red', 'adjective'),
+('синий', 'blue', 'adjective'),
+('зелёный', 'green', 'adjective'),
+('жёлтый', 'yellow', 'adjective'),
+('чёрный', 'black', 'adjective'),
+('белый', 'white', 'adjective'),
+('я', 'I', 'pronoun'),
+('ты', 'you', 'pronoun'),
+('он', 'he', 'pronoun'),
+('она', 'she', 'pronoun');
